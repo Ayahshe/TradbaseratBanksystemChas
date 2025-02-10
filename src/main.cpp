55620524bbf89389c718dd8fcc1a587899685235
linuschas <linus.nilsson@chasacademy.se>
2025-02-10 11:01:30 +0100

    feat(account & client amount prompt)

--- src/main.cpp
+++ src/main.cpp
@@ -83,8 +83,26 @@ void Client(Bank& bank, int clientId) {
 int main() {
     Bank bank;
 
+    int totalAccounts(0);
+    std::cout << "How many bank accounts do you want?" << std::endl;
+    std::cin >> totalAccounts;
+    if (!totalAccounts || totalAccounts < 1 || totalAccounts > 500) {
+        std::cout << "Invalid account amount. Choose between 1-500." << std::endl;
+
+        return 1;
+    }
+
+    int totalClients(0);
+    std::cout << "How many clients do you want?" << std::endl;
+    std::cin >> totalClients;
+    if (!totalClients || totalClients < 1 || totalClients > 500) {
+        std::cout << "Invalid client amount. Choose between 1-500." << std::endl;
+
+        return 1;
+    }
+
     // Skapa och lägg till 5 konton
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < totalAccounts; ++i) {
         int accountNumber = bank.generateAccountNumber();
         BankAccount account(accountNumber, 100 * (i + 1)); // Skapa BankAccount-objekt
         bank.addAccount(account); // Lägg till kontot i banken
@@ -92,7 +110,7 @@ int main() {
 
     // Starta klienttrådar
     std::vector<std::thread> clients;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < totalClients; ++i) {
         clients.emplace_back(Client, std::ref(bank), i);
     }
 
